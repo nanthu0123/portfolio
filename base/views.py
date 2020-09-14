1c264b0cfcd1adb05cd0667dd6784248c6f2f57f
nanthu0123 <nanthu0123@gmail.com>
2020-09-14 16:17:55 +0530

    dummy post contents

--- base/views.py
+++ base/views.py
@@ -9,6 +9,15 @@ def home(request):
 
 
 def posts(request):
+    posts=[
+        {'headline':'calculator',
+        'sub_headline':'this is a calculator app'},
+        {'headline':'weather App',
+        'sub_headline':'this is weather app to prodict weather changes'},
+        {'headline':'todo list',
+        'sub_headline':'this is the todo list to remember things'}
+    ]
+    context={'posts':posts}
     return render(request, 'base/posts.html')
 
 
