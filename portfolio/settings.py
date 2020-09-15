df0c4b2eb906902e69f9f8ff919d5a3495e2468f
nanthu0123 <nanthu0123@gmail.com>
2020-09-15 20:43:48 +0530

    new installed apps

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'base.apps.BaseConfig',
+    'crispy_forms',
+    'django_filters',
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
