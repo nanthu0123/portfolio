780343391a419c2cbad0a5e0dd05a083b7a2108c
nanthu0123 <nanthu0123@gmail.com>
2020-09-15 20:40:30 +0530

    logic developed in views file

--- base/views.py
+++ base/views.py
@@ -1,29 +1,79 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# Create your views here.
+from .models import Post
+from .forms import PostForm
+from .filters import PostFilter
 
 
 def home(request):
-    return render(request, 'base/index.html')
+    posts = Post.objects.filter(active=True, featured=True)[0:3]
+    context = {'posts': posts}
+    return render(request, 'base/index.html', context)
 
 
 def posts(request):
-    posts=[
-        {'headline':'calculator',
-        'sub_headline':'this is a calculator app'},
-        {'headline':'weather App',
-        'sub_headline':'this is weather app to prodict weather changes'},
-        {'headline':'todo list',
-        'sub_headline':'this is the todo list to remember things'}
-    ]
-    context={'posts':posts}
-    return render(request, 'base/posts.html')
+    posts = Post.objects.filter(active=True)
+    myFilter = PostFilter(request.GET, queryset=posts)
+    posts = myFilter.qs
+
+    page = request.GET.get('page')
+    paginator = Paginator(posts, 3)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    context = {'posts': posts, 'myFilter': myFilter}
+    return render(request, 'base/posts.html', context)
 
 
-def post(request):
-    return render(request, 'base/post.html')
+def post(request, pk):
+    post = Post.objects.get(id=pk)
+    context = {'post': post}
+    return render(request, 'base/post.html', context)
 
 
 def profile(request):
     return render(request, 'base/profile.html')
+
+# CURD VIEW
+
+
+@login_required(login_url='home')
+def createPost(request):
+    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('posts')
+    context = {'form': form}
+    return render(request, 'base/post_form.html', context)
+
+
+@login_required(login_url='home')
+def updatePost(request, pk):
+    post = Post.objects.get(id=pk)
+    form = PostForm(instance=post)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+        return redirect('posts')
+    context = {'form': form}
+    return render(request, 'base/post_form.html', context)
+
+
+@login_required(login_url='home')
+def deletePost(request, pk):
+    post = Post.objects.get(id=pk)
+    context = {'item': post}
+    if request.method == 'POST':
+        post.delete()
+        return redirect('posts')
+    return render(request, 'base/delete.html', context)
