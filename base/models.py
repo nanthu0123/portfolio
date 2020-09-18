ea6018a5f568ab1a673077efcfb7c9f6c3b07a6b
nanthu0123 <nanthu0123@gmail.com>
2020-09-18 21:50:44 +0530

    ckeditor_uploader for upload static imgs

--- base/models.py
+++ base/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class Tag(models.Model):
@@ -15,7 +16,7 @@ class Post(models.Model):
     sub_headline = models.CharField(max_length=200, null=True, blank=True)
     thumbnail = models.ImageField(
         null=True, blank=True, upload_to='images', default='blur-img.png')
-    body = RichTextField(null=True, blank=True)
+    body = RichTextUploadingField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=False)
     featured = models.BooleanField(default=False)
