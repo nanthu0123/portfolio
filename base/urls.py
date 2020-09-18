163f8988e7fb0884fe8c0e244ab655b686352851
nanthu0123 <nanthu0123@gmail.com>
2020-09-18 20:59:10 +0530

    change pk to slug in urls

--- base/urls.py
+++ base/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('post/<str:pk>', views.post, name='post'),
+    path('post/<slug:slug>', views.post, name='post'),
     path('posts/', views.posts, name='posts'),
     path('profile/', views.profile, name='profile'),
     # CURD PATH
     path('create_post/', views.createPost, name='create_post'),
-    path('update_post/<str:pk>', views.updatePost, name='update_post'),
-    path('delete_post/<str:pk>', views.deletePost, name='delete_post'),
+    path('update_post/<slug:slug>', views.updatePost, name='update_post'),
+    path('delete_post/<slug:slug>', views.deletePost, name='delete_post'),
 
     path('send_email', views.sendEmail, name='send_email'),
 ]
