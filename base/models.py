f2c3c220ced71536c9606b8f5a45986b918b8c08
nanthu0123 <nanthu0123@gmail.com>
2020-09-20 23:30:04 +0530

    default img path changed

--- base/models.py
+++ base/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     headline = models.CharField(max_length=200)
     sub_headline = models.CharField(max_length=200, null=True, blank=True)
     thumbnail = models.ImageField(
-        null=True, blank=True, upload_to='images', default='blur-img.png')
+        null=True, blank=True, upload_to='images', default='/images/blur-img.png')
     body = RichTextUploadingField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=False)
